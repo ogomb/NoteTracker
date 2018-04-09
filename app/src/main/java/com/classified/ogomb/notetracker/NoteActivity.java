560a7990d9a8f0e56a7f888223200ce31a5e9366
ogomb <mbogolewis@gmail.com>
2018-04-09 15:51:12 +0300

    create note list activity and fragment.

--- app/src/main/java/com/classified/ogomb/notetracker/NoteActivity.java
+++ app/src/main/java/com/classified/ogomb/notetracker/NoteActivity.java
@@ -6,23 +6,10 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 
 
-public class NoteActivity extends FragmentActivity {
+public class NoteActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-
-        FragmentManager manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null){
-            fragment = new NoteFragment();
-            manager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
-
+    public Fragment createFragment() {
+        return new NoteFragment();
     }
 }
---
+++ app/src/main/java/com/classified/ogomb/notetracker/NoteListFragment.java
@@ -0,0 +1,10 @@
+package com.classified.ogomb.notetracker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by lewismbogo on 09/04/2018.
+ */
+
+public class NoteListFragment extends Fragment {
+}
