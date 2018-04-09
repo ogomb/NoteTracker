05f21121faf7ab6103eeb82816b9f24f6ff52531
ogomb <mbogolewis@gmail.com>
2018-04-09 12:25:04 +0300

    add edit text widget container for notes.

--- app/src/main/java/com/classified/ogomb/notetracker/NoteActivity.java
+++ app/src/main/java/com/classified/ogomb/notetracker/NoteActivity.java
@@ -2,12 +2,18 @@ package com.classified.ogomb.notetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NoteActivity extends AppCompatActivity {
+    private EditText mNoteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        mNoteText = (EditText) findViewById(R.id.noteText);
+
+
     }
 }
