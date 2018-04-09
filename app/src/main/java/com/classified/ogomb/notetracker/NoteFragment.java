c726108ed2667cefadd25751ae7579c64a452e42
ogomb <mbogolewis@gmail.com>
2018-04-09 15:17:37 +0300

    add text changed listener to the edit text.

--- app/src/main/java/com/classified/ogomb/notetracker/NoteFragment.java
+++ app/src/main/java/com/classified/ogomb/notetracker/NoteFragment.java
@@ -2,18 +2,21 @@ package com.classified.ogomb.notetracker;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class NoteFragment  extends Fragment {
-
+    private Note  mNote;
     private EditText mNoteText;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        mNote = new Note();
     }
 
     @Override
@@ -21,6 +24,22 @@ public class NoteFragment  extends Fragment {
         View view = inflater.inflate(R.layout.fragment_note,container,false);
 
         mNoteText = view.findViewById(R.id.noteText);
+        mNoteText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mNote.setNote((String) charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         return view;
     }
 
